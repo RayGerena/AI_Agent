010f617cb51302fb6a65fc317fe86e601923d874
RayGerena <raygerena23@gmail.com>
2025-07-05 19:06:16 -0400

    Added read, write, execute functions to agent

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,20 +1,38 @@
 import os
+from google.genai import types
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory, file_path):
-    wd = os.path.abspath(working_directory)
-    filepath = os.path.abspath(os.path.join(wd, file_path))
-    if not filepath.startswith(wd):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(filepath):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        MAX_CHARS = 10000
-
-        with open(filepath, "r") as f:
-            file_content_string = f.read(MAX_CHARS + 1)
-            if len(file_content_string) > MAX_CHARS:
-                file_content_string = file_content_string[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at 10000 characters]'
-            return file_content_string
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f"Error reading file: {e}"
+        return f'Error reading file "{file_path}": {e}'
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
